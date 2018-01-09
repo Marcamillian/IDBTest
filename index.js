09ef6dc3ceb9abe3c496d8f5538e2dfd30e29ce3
Marcamillian <marcamillian@gmail.com>
2018-01-09 16:34:03 +0000

    Skeleton setup to start using idb

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const http = require('http')
+
+const app = express();
+let server;
+
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static('./src/client'))
+
+server = http.createServer(app);
+
+server.listen(app.get('port'), ()=>{
+    console.log(`Node app is running on port ${app.get('port')}`)
+})

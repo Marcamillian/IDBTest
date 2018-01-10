55bd477d162160a8861a905cb29e2276e955c997
Marcamillian <marcamillian@gmail.com>
2018-01-10 22:37:08 +0000

    Adding items into same objectstore with a lsit field to separate them

--- src/main.js
+++ src/main.js
@@ -9,47 +9,46 @@ const listName = document.querySelector('#list-name')
 const listCreateButton = document.querySelector('button#list-create')
 const listShowButton = document.querySelector('button#list-show')
 
+// things to do with the list
+const listNames = new Set(['firstList'])
+let activeList = 'firstList'
+
 
 var dbPromise = idb.open('spend-lists',1, (upgradeDb)=>{
-
-    // check which lists are already there
-
-    // decide on what the new list is OR open the last
-
-    var listStore = upgradeDb.createObjectStore('lists', 1 , {keyPath:'id'});
-    listStore.put([{description: "hamburger", cost: 2.40}])
-    
+    var listStore = upgradeDb.createObjectStore('purchased-items', {autoIncrement: true});
+    listStore.createIndex('by-list', "listName")
 })
 
 
 // utility functions
-const addRecord = (listKey, description, cost)=>{
-    return getList("firstList").then((listArray)=>{
-        listArray.push({description: description, cost:cost })
-        return listArray 
-    }).then((updatedList)=>{
-        return dbPromise.then((db)=>{
-            var tx = db.transaction('lists', 'readwrite')
-            var listStore = tx.objectStore('lists')
-            listStore.put(updatedList, "firstList")
-            return tx.complete;
-        })
+const addRecord = (listName, description, cost)=>{
+
+    console.log(`description: ${description} cost: ${cost}`)
+    return dbPromise.then((db)=>{
+        var tx = db.transaction('purchased-items', 'readwrite')
+        var listStore = tx.objectStore('purchased-items')
+        listStore.put( {listName: listName, description: description, price: cost})
+        return tx.complete;
     })
 }
+try{
+    addRecord("someList", "hat", 50).then(()=>{
+        console.log("added a default thing")
+    })
+}catch(err){
+    console.log(`something couldn't be added ${err.message}`)
+}
+
 
 var createList = (listName)=>{
-    return dbPromise.then(function(db){
-        var tx= db.transaction('lists', 'readwrite')
-        var listStore = tx.objectStore('lists')
-        listStore.put({}, listName)
-        return tx.complete
-    })
+    listNames.push(listName)
+
 }
 
 var getList = (listKey)=>{
     return dbPromise.then((db)=>{
-        var tx = db.transaction('lists');
-        var listStore = tx.objectStore('lists')
+        var tx = db.transaction('purchased-items');
+        var listStore = tx.objectStore('purchased-items')
         return listStore.get(listKey)
     })
 }
@@ -68,9 +67,9 @@ let getListNames =()=>{
 
 inputRecord.addEventListener('click', ()=>{
     console.log("adding a record")
-
+    console.log(`active list : ${activeList}`)
     //console.log(`key: ${recordKey.value} || value: ${recordValue.value}`)
-    addRecord('firstList',  "hat", 34).then(()=>{console.log("record added")})
+    addRecord(activeList, itemDescription.value, itemValue.value ).then(()=>{console.log("record added")})
     //addRecord('keyval',itemDescription.value, itemValue.value)
 })
 

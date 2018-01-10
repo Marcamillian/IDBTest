7867f82e73347fad060263e542b0712c2b4e0fd8
Marcamillian <marcamillian@gmail.com>
2018-01-10 12:53:36 +0000

    Basics of lists being the key in the database and the list beign an array. Adding to the list

--- src/main.js
+++ src/main.js
@@ -1,65 +1,66 @@
 const idb = require('idb');
 
-let dbPromise = idb.open('spend-list', 4, (upgradeDb)=>{
-    switch(upgradeDb.oldVersion){
-        case 0:
-            var keyValStore = upgradeDb.createObjectStore('keyval');
-            keyValStore.put("world", "hello")
-    }
-})
-
 // DOM elements
 const inputRecord = document.querySelector('button')
-const recordKey = document.querySelector('#record-key')
-const recordValue = document.querySelector('#record-value')
+const itemDescription = document.querySelector('#item-description')
+const itemValue = document.querySelector('#item-value')
 
 const listName = document.querySelector('#list-name')
 const listCreateButton = document.querySelector('button#list-create')
 
+var dbPromise = idb.open('spend-lists',1, (upgradeDb)=>{
+
+    // check which lists are already there
+
+    // decide on what the new list is OR open the last
+
+
+    switch(upgradeDb.oldVersion){
+        case 0:
+            var keyValStore = upgradeDb.createObjectStore('lists');
+            keyValStore.put([{description: "hamburger", cost: 2.40}], "firstList")
+    }
+})
+
+
 // utility functions
-const addRecord = (storeKey, key, value)=>{
-    return dbPromise.then(function(db){
-        var tx = db.transaction('keyval', 'readwrite')
-        var keyvalStore = tx.objectStore('keyval')
-        
-        keyvalStore.put(value, key)
-        return tx.complete
+const addRecord = (listKey, description, cost)=>{
+    return getList("firstList").then((listArray)=>{
+        listArray.push({description: description, cost:cost })
+        return listArray 
+    }).then((updatedList)=>{
+        return dbPromise.then((db)=>{
+            var tx = db.transaction('lists', 'readwrite')
+            var keyvalStore = tx.objectStore('lists')
+            keyvalStore.put(updatedList, "firstList")
+            return tx.complete;
+        })
     })
 }
 
-const createList = (listName)=>{
-    return idb.open('spend-list', 4, (upgradeDb)=>{
-        
-        // create a new list with the list name
-        var newStore = upgradeDb.createObjectStore(listName)
-        newStore.put("new", "store")
+var createList = (listName)=>{
+    return dbPromise.then(function(db){
+        var tx= db.transaction('keyval')
     })
 }
 
-const showRecords = ()=>{
-    return dbPromise.then(function(db){
-        var tx = db.transaction('keyval')
-        var keyvalStore = tx.objectStore('keyval')
-        keyvalStore.getAll()
-    }).then( (wholeList) => {
-        console.log(`all of the things in the list ${wholeList}`)
+var getList = (listKey)=>{
+    return dbPromise.then((db)=>{
+        var tx = db.transaction('lists');
+        var keyvalStore = tx.objectStore('lists')
+        return keyvalStore.get(listKey)
     })
 }
 
 inputRecord.addEventListener('click', ()=>{
     console.log("adding a record")
 
-    console.log(`key: ${recordKey.value} || value: ${recordValue.value}`)
-
-    //addRecord('keyval',recordKey.value, recordValue.value)
-
-    createList("something")
+    //console.log(`key: ${recordKey.value} || value: ${recordValue.value}`)
+    addRecord('firstList',  "hat", 34).then(()=>{console.log("record added")})
+    //addRecord('keyval',itemDescription.value, itemValue.value)
 })
 
 
 listCreateButton.addEventListener('click',()=>{
-    console.log("trying to create a list")
-    //dbPromise = createList("someList")
 
-    showRecords()
 })
